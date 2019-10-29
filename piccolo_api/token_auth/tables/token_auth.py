0e3dd9d60a5e51029473fa15a5642c1c404a4b11
Daniel Townsend <daniel.townsend@worldspan.co.uk>
2019-10-29 17:00:21 +0000

    fleshing out token auth

---
+++ piccolo_api/token_auth/tables/token_auth.py
@@ -0,0 +1,20 @@
+import secrets
+
+from piccolo.columns.column_types import Varchar, ForeignKey
+from piccolo.extensions.user import BaseUser
+from piccolo.table import Table
+
+
+class TokenAuth(Table):
+    """
+    Holds randomly generated tokens.
+
+    Useful for mobile authentication, IOT etc. Session auth is recommended for
+    web usage.
+    """
+    token = Varchar()
+    user = ForeignKey(references=BaseUser)
+
+    @classmethod
+    def create_token(user_id: int):
+        pass
