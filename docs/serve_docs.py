46f52a1cbcb76b07ca2a661112a25004adce4f26
Daniel Townsend <dan@dantownsend.co.uk>
2021-03-24 21:21:10 +0000

    add linters to github actions

--- docs/serve_docs.py
+++ docs/serve_docs.py
@@ -3,13 +3,6 @@ from livereload import Server, shell
 
 
 server = Server()
-server.watch(
-    'source/',
-    shell('make html')
-)
-server.watch(
-    '../piccolo_api',
-    shell('make html')
-)
-server.serve(root='build/html')
-
+server.watch("source/", shell("make html"))
+server.watch("../piccolo_api", shell("make html"))
+server.serve(root="build/html")
