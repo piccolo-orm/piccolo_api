07e094e26d5ffdae0b259261cdd8fdd560a1afea
Daniel Townsend <dan@dantownsend.co.uk>
2021-08-09 10:58:30 +0100

    fix serve.py
    This was a forgotten script, which @sinisaos rightly changed because it no longer worked, and the types were failing. I've restored it to so it works as originally intended.

--- tests/serve.py
+++ tests/serve.py
@@ -1,16 +1,25 @@
 """
 Used for serving the example session app, so we can manually test it.
+
+Run it from the root of the project using `python -m tests.serve`.
 """
+import os
+
+import uvicorn
+
+from .session_auth.test_session import (  # type: ignore
+    APP,
+    BaseUser,
+    SessionsBase,
+)
 
 if __name__ == "__main__":
-    import os
-    import sys
+    os.environ["PICCOLO_CONF"] = "tests.sqlite_conf"
 
-    import uvicorn
-    from session_auth.test_session import APP  # type: ignore
+    BaseUser.create_table(if_not_exists=True).run_sync()
+    SessionsBase.create_table(if_not_exists=True).run_sync()
 
-    sys.path.append(
-        os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    )
+    if not BaseUser.exists().where(BaseUser.username == "Bob"):
+        BaseUser(username="bob", password="bob123").save().run_sync()
 
     uvicorn.run(APP, port=8999)
