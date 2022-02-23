cdceb0f2cc035e97d837b811460e72de6e719e97
Daniel Townsend <dan@dantownsend.co.uk>
2022-02-23 10:17:02 +0000

    user new docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,22 +38,11 @@ master_doc = "index"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx_autodoc_typehints"]
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+extensions = ["sphinx.ext.autodoc"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
-html_logo = "logo.png"
-html_theme_options = {"logo_only": True}
+html_theme = "piccolo_theme"
