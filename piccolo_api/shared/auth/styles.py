3e6af6bd197cbe302c3c54504e67e2bd674a2721
Daniel Townsend <dan@dantownsend.co.uk>
2022-06-05 14:05:36 +0100

    improve docstring for `Styles`

--- piccolo_api/shared/auth/styles.py
+++ piccolo_api/shared/auth/styles.py
@@ -3,6 +3,14 @@ from dataclasses import dataclass
 
 @dataclass
 class Styles:
+    """
+    Used to set CSS styles in endpoints such as
+    :func:`session_login <piccolo_api.session_auth.endpoints.session_login>`,
+    :func:`session_logout <piccolo_api.session_auth.endpoints.session_logout>`,
+    and :func:`register <piccolo_api.register.endpoints.register>`.
+
+    """
+
     background_color: str = "#eef2f5"
     foreground_color: str = "white"
     text_color: str = "black"
