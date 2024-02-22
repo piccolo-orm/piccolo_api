10c1c2eedc9cda0de33987a93bf086a4d6cf83ab
Daniel Townsend <dan@dantownsend.co.uk>
2024-02-22 12:36:44 +0000

    fix rate limiting test

--- tests/rate_limiting/test_rate_middleware.py
+++ tests/rate_limiting/test_rate_middleware.py
@@ -1,10 +1,11 @@
+import asyncio
 from time import sleep
 from unittest import TestCase
 
+from httpx import ASGITransport, AsyncClient
 from starlette.endpoints import HTTPEndpoint
 from starlette.responses import JSONResponse
 from starlette.routing import Route, Router
-from starlette.testclient import TestClient
 
 from piccolo_api.rate_limiting.middleware import (
     InMemoryLimitProvider,
@@ -27,23 +28,36 @@ class TestMiddleware(TestCase):
             InMemoryLimitProvider(limit=5, timespan=1, block_duration=1),
         )
 
-        client = TestClient(app)
+        # We have to use `httpx.AsyncClient` directly, because `TestClient`
+        # was broken in this PR:
+        # https://github.com/encode/starlette/pull/2377
+        # `TestClient` no longer sends the client IP and port.
+        # If a fix is released, we can go back to using `TestClient` directly.
+        client = AsyncClient(
+            transport=ASGITransport(app=app),
+            base_url="http://testserver",
+            timeout=10.0,
+            follow_redirects=True,
+        )
 
-        successful = 0
-        for i in range(20):
-            response = client.get("/")
-            if response.status_code == 429:
-                break
-            else:
-                successful += 1
+        async def run_test():
+            successful = 0
+            for _ in range(20):
+                response = await client.get("/")
+                if response.status_code == 429:
+                    break
+                else:
+                    successful += 1
 
-        self.assertEqual(successful, 5)
+            self.assertEqual(successful, 5)
 
-        # After the 'block_duration' has expired, requests should be allowed
-        # again.
-        sleep(1.1)
-        response = client.get("/")
-        self.assertEqual(response.status_code, 200)
+            # After the 'block_duration' has expired, requests should be
+            # allowed again.
+            sleep(1.1)
+            response = await client.get("/")
+            self.assertEqual(response.status_code, 200)
+
+        asyncio.run(run_test())
 
     def test_memory_usage(self):
         """
