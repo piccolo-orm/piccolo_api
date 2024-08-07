5bf4a2d1aaab8e6dcb80fe7297e1c3f2049a2714
Daniel Townsend <dan@dantownsend.co.uk>
2024-08-07 18:18:05 +0100

    flesh out `AuthenticatorSeed` some more

--- piccolo_api/mfa/authenticator/tables.py
+++ piccolo_api/mfa/authenticator/tables.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+import datetime
+
 from piccolo.columns import Integer, Serial, Text, Timestamptz
 from piccolo.table import Table
 
@@ -6,4 +10,35 @@ class AuthenticatorSeed(Table):
     id: Serial
     user_id = Integer(null=False)
     code = Text()
+    revoked_at = Timestamptz(null=True, default=None)
     created_at = Timestamptz()
+    last_used_at = Timestamptz()
+
+    @classmethod
+    async def create_new(cls, user_id: int) -> AuthenticatorSeed:
+        # TODO - generate proper code
+        instance = cls({cls.user_id: user_id, cls.code: "ABC123"})
+        await instance.save()
+        return instance
+
+    @classmethod
+    async def authenticate(cls, user_id: int, code: str) -> bool:
+        seeds = cls.objects().where(
+            cls.user_id == user_id,
+            cls.revoked_at.is_null(),
+        )
+
+        # We check all seeds - a user is allowed multiple seeds (i.e. if they
+        # have multiple devices).
+
+        for seed in seeds:
+            # TODO - add the proper code checking
+            if seed.code == "abc123":
+                seed.last_used_at = datetime.datetime.now(
+                    tz=datetime.timezone.utc
+                )
+                await seed.save(columns=[cls.last_used_at])
+
+                return True
+
+        return False
