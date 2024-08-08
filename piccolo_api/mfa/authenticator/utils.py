f4c8bf0a5825905c751648921b60186cece98c19
Daniel Townsend <dan@dantownsend.co.uk>
2024-08-08 19:59:35 +0100

    lazy load `qrcode`

--- piccolo_api/mfa/authenticator/utils.py
+++ piccolo_api/mfa/authenticator/utils.py
@@ -1,10 +1,29 @@
+from __future__ import annotations
+
+import typing as t
 from base64 import b64encode
 from io import BytesIO
 
-import qrcode
+if t.TYPE_CHECKING:
+    import qrcode
+
+
+def get_qrcode() -> qrcode:
+    try:
+        import qrcode
+    except ImportError as e:
+        print(
+            "Install pip install piccolo_api[authenticator] to use this "
+            "feature."
+        )
+        raise e
+
+    return qrcode
 
 
 def get_b64encoded_qr_image(data):
+    qrcode = get_qrcode()
+
     qr = qrcode.QRCode(version=1, box_size=4, border=5)
     qr.add_data(data)
     qr.make(fit=True)
