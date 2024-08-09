c1b1c059ecd50e9bb3282f15d81b02a042e7bf78
Daniel Townsend <dan@dantownsend.co.uk>
2024-08-09 11:21:52 +0100

    flesh out `MFARegisterEndpoint` endpoint

--- piccolo_api/mfa/endpoints.py
+++ piccolo_api/mfa/endpoints.py
@@ -1,7 +1,34 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 
 from starlette.endpoints import HTTPEndpoint
+from starlette.requests import Request
+from starlette.responses import HTMLResponse, JSONResponse
+
+from piccolo_api.mfa.provider import MFAProvider
 
 
 class MFARegisterEndpoint(HTTPEndpoint, metaclass=ABCMeta):
-    pass
+
+    @property
+    @abstractmethod
+    def _provider(self) -> MFAProvider:
+        raise NotImplementedError
+
+    async def get(self, request: Request):
+        if request.query_params.get("format") == "json":
+            return HTMLResponse(content=self._provider.get_registration_html())
+        else:
+            return JSONResponse(content=self._provider.get_registration_json())
+
+    async def post(self, request: Request):
+        # TODO - we might need the user to confirm once they're setup.
+        # We could embed the ID of the row in the HTML response (in a form).
+        pass
+
+
+def mfa_register_endpoint(provider: MFAProvider) -> HTTPEndpoint:
+
+    class _MFARegisterEndpoint(MFARegisterEndpoint):
+        _provider = provider
+
+    return _MFARegisterEndpoint
