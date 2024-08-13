64ef81b5fd588cab1a4890ba83287c72482a3632
Daniel Townsend <dan@dantownsend.co.uk>
2024-08-13 14:11:30 +0100

    update `TestMFARegisterEndpoint`

--- tests/mfa/test_mfa_endpoints.py
+++ tests/mfa/test_mfa_endpoints.py
@@ -1,30 +1,44 @@
+from piccolo.apps.user.tables import BaseUser
 from piccolo.testing.test_case import AsyncTableTest
-from starlette.applications import Starlette
-from starlette.routing import Route
 from starlette.testclient import TestClient
 
-from piccolo_api.mfa.authenticator.provider import AuthenticatorProvider
+from example_projects.mfa_demo.app import app
 from piccolo_api.mfa.authenticator.tables import AuthenticatorSecret
-from piccolo_api.mfa.endpoints import mfa_register_endpoint
+from piccolo_api.session_auth.tables import SessionsBase
 
 
 class TestMFARegisterEndpoint(AsyncTableTest):
 
-    tables = [AuthenticatorSecret]
+    tables = [AuthenticatorSecret, BaseUser, SessionsBase]
+    username = "alice"
+    password = "test123"
 
-    async def test_register(self):
-        # Rather than setting all of this up ... what if I use the example app?
-        app = Starlette(
-            routes=[
-                Route(
-                    path="/register/",
-                    endpoint=mfa_register_endpoint(
-                        provider=AuthenticatorProvider()
-                    ),
-                )
-            ]
+    async def asyncSetUp(self) -> None:
+        await super().asyncSetUp()
+
+        self.user = await BaseUser.create_user(
+            username=self.username, password=self.password, active=True
         )
 
+    async def test_register_json(self):
         client = TestClient(app=app)
 
-        client.get("/register/")
+        # Get a CSRF cookie
+        response = client.get("/login/")
+        csrf_token = response.cookies["csrftoken"]
+        self.assertEqual(response.status_code, 200)
+
+        # Login
+        response = client.post(
+            "/login/",
+            json={"username": self.username, "password": self.password},
+            headers={"X-CSRFToken": csrf_token},
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("id", client.cookies)
+
+        # TODO - rather than a GET param, can we pass in a content header?
+        response = client.get("/private/mfa-register/?format=json")
+        data = response.json()
+        self.assertIn("qrcode_image", data)
+        self.assertIn("recovery_codes", data)
