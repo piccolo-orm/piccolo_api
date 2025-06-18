c96995afb8c2c117e3cce1d3f93e949a4ca8a9e1
Andrew Shao <132319777+andrewshaodev@users.noreply.github.com>
2025-06-18 05:55:39 -0400

    Add BaseUser import from piccolo.apps.user.tables (#313)
    * Add BaseUser import from piccolo.apps.user.tables
    
    Imported BaseUser from piccolo.apps.user.tables for use in authentication and JWT middleware integration. This enables the app to validate users against the default Piccolo user table when protecting routes with JWTMiddleware.
    
    * move table import to bottom
    
    ---------
    
    Co-authored-by: Daniel Townsend <dan@dantownsend.co.uk>

--- docs/source/jwt/examples/example.py
+++ docs/source/jwt/examples/example.py
@@ -1,13 +1,16 @@
 from datetime import timedelta
 
 from fastapi import FastAPI, Request
-from home.tables import Movie  # An example Table
 from piccolo_admin.endpoints import create_admin
 from piccolo_api.crud.endpoints import PiccoloCRUD
 from piccolo_api.fastapi.endpoints import FastAPIKwargs, FastAPIWrapper
 from piccolo_api.jwt_auth.endpoints import jwt_login
 from piccolo_api.jwt_auth.middleware import JWTBlacklist, JWTMiddleware
+from piccolo.apps.user.tables import BaseUser
 from starlette.routing import Mount, Route
+
+from home.tables import Movie  # An example Table
+
 
 public_app = FastAPI(
     routes=[
@@ -53,6 +56,7 @@ FastAPIWrapper(
 )
 
 public_app.mount("/private", protected_app)
+
 
 # This is optional if you want to provide a logout endpoint
 # in your application. By adding a token to the token blacklist,
